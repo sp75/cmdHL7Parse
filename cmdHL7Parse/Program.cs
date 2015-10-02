c4168a234a8f03a34a424a85b2a9cd28f99381af
sp75 <palamarchuk-sv@mail.ru>
2015-10-02 10:57:54 +0300

    +

--- cmdHL7Parse/Program.cs
+++ cmdHL7Parse/Program.cs
@@ -17,7 +17,7 @@ namespace cmdHL7Parse
         private static void Main( string[] args )
         {
 
-            Console.WriteLine( "*** HL7 Parse utility ***" );
+         /*   Console.WriteLine( "*** HL7 Parse utility ***" );
             foreach ( var s in args )
             {
                 Console.WriteLine( s );
@@ -40,9 +40,10 @@ namespace cmdHL7Parse
                 Console.WriteLine( "ERROR: File config.ini doesn't exist" );
                 return;
             }
-            FileName = args[ 0 ];
+            FileName = args[ 0 ];*/
 
-          //      FileName = "RPRresult(1).exp";
+            FileName = "RPRresult_for testing probable pregnancy.exp";
+
 
             var dd = HL7.ParseMSG(FileName);
             GetDataFromHistory3( dd );
@@ -57,6 +58,14 @@ namespace cmdHL7Parse
             GetDataFromHistory();
             GetData2();
              */
+        }
+
+        private static string[] GetConfig()
+        {
+            return
+                File.ReadAllLines( "config.ini" )
+                    .Where( s => !String.IsNullOrEmpty( s ) ).Where( s => s.Trim()[ 0 ] != ';' )
+                    .ToArray();
         }
 
         private static void GetDataFromHistory3(List<MSG> msg)
@@ -88,14 +97,17 @@ namespace cmdHL7Parse
             }
         }
 
+        static List<String> _obx = new List<string>();
         private static void GetData3( List<MSG> msg )
         {
             int a = 0, countMsg = msg.Count, pr = 0;
 
-            string[] configLines = File.ReadAllLines( "config.ini" );
+        //    string[] configLines = File.ReadAllLines( "config.ini" );
+            var configLines = GetConfig();
 
             foreach ( var msg_item in msg )
             {
+                _obx.Clear();
                 string[] p_name = msg_item.PID_split[ 5 ].Split( '^' );
 
                 int y = !String.IsNullOrEmpty( msg_item.PID_split[ 7 ] )
@@ -123,24 +135,23 @@ namespace cmdHL7Parse
 
                                     foreach ( string line in configLines )
                                     {
-                                        if ( !String.IsNullOrEmpty( line ) )
+
+                                        string[] clinesplit = line.Split( '|' );
+                                        string[] configkay = clinesplit[ 0 ].Split( '^' );
+                                        int config_year = clinesplit.Count() > 2
+                                            ? Convert.ToInt32( clinesplit[ 2 ] )
+                                            : -1;
+                                        if ( configkay[ 0 ] == test &&
+                                             configkay[ 1 ] == obx_item.OBX_split_heder[ 8 ] &&
+                                             ( config_year == -1 || y <= config_year ) )
                                         {
-                                            string[] clinesplit = line.Split( '|' );
-                                            string[] configkay = clinesplit[ 0 ].Split( '^' );
-                                            int config_year = clinesplit.Count() > 2
-                                                ? Convert.ToInt32( clinesplit[ 2 ] )
-                                                : -1;
-                                            if ( configkay[ 0 ] == test &&
-                                                 configkay[ 1 ] == obx_item.OBX_split_heder[ 8 ] &&
-                                                 ( config_year == -1 || y <= config_year ) )
-                                            {
-                                                CheckOtherResults2( msg,
-                                                    msg_item.PID_split[ 2 ],
-                                                    clinesplit[ 1 ].Split( '^' ),
-                                                    clinesplit[ 1 ],
-                                                    clinesplit );
-                                            }
+                                            CheckOtherResults2( msg,
+                                                msg_item.PID_split[ 2 ],
+                                                clinesplit[ 1 ].Split( '^' ),
+                                                clinesplit[ 1 ],
+                                                clinesplit );
                                         }
+
                                     }
                                 }
                             }
@@ -161,50 +172,59 @@ namespace cmdHL7Parse
 
         static void CheckOtherResults2(List<MSG> msg, string pid, string[] Tests, string test, string[] confog_line)
         {
-            string[] p_4 = confog_line[3].Split('^');
-
+            string key = confog_line[ 0 ].Split( '^' )[ 0 ];
             foreach ( var msg_item in msg )
             {
                 if ( msg_item.PID_split[ 2 ] == pid )
                 {
-                    bool pt = PregnancyTest( p_4, msg_item.OBRs );
+                    bool pt = confog_line.Count() == 4
+                        ? PregnancyTest( confog_line[ 3 ].Split( '^' ), msg_item.OBRs )
+                        : false;
 
                     foreach ( var obr_item in msg_item.OBRs )
                     {
-                        if (pt)
-                        {
-                            obr_item.OBR_split[13] = "PROBABLE PREGNANCY";
-                            obr_item.OBR_line = String.Join("|", obr_item.OBR_split);
-                        }
+
 
                         foreach ( var zlr_item in obr_item.ZLRs )
                         {
                             String logobx = "";
-                            foreach (var obx_item in zlr_item.OBXs)
+                            foreach ( var obx_item in zlr_item.OBXs )
                             {
-                                string[] ObservationIdentifier = obx_item.OBX_split_heder[3].Split('^');
-                                String id_test = ObservationIdentifier[0];
-                                
-                                if (Tests.Contains(id_test))
+                                string[] ObservationIdentifier = obx_item.OBX_split_heder[ 3 ].Split( '^' );
+                                String id_test = ObservationIdentifier[ 0 ];
+
+                                if ( pt && id_test == key ) //
                                 {
-                                    String oi_1 = ObservationIdentifier[0];
-                                    String oi_4 = ObservationIdentifier[3];
-
-                                    String oi_3 = ObservationIdentifier[2];
-                                    String oi_6 = ObservationIdentifier[5];
-                                    ObservationIdentifier[0] = oi_4;
-                                    ObservationIdentifier[3] = oi_1;
-
-                                    ObservationIdentifier[2] = oi_6;
-                                    ObservationIdentifier[5] = oi_3;
-                                    obx_item.OBX_split_heder[3] = String.Join("^", ObservationIdentifier);
-
-                                    logobx += String.Join("|", obx_item.OBX_split_heder) + Environment.NewLine;
+                                    obr_item.OBR_split[ 13 ] = "PROBABLE PREGNANCY";
+                                    obr_item.OBR_line = String.Join( "|", obr_item.OBR_split );
                                 }
-                             
+                                var tmp_ = obx_item.OBX_split_heder.ToArray();
+                                if ( Tests.Contains( id_test ) )
+                                {
+                                    String oi_1 = ObservationIdentifier[ 0 ];
+                                    String oi_4 = ObservationIdentifier[ 3 ];
+
+                                    String oi_3 = ObservationIdentifier[ 2 ];
+                                    String oi_6 = ObservationIdentifier[ 5 ];
+                                    ObservationIdentifier[ 0 ] = oi_4;
+                                    ObservationIdentifier[ 3 ] = oi_1;
+
+                                    ObservationIdentifier[ 2 ] = oi_6;
+                                    ObservationIdentifier[ 5 ] = oi_3;
+                                    /* obx_item.OBX_split_heder[3]*/
+                                    tmp_[ 3 ] = String.Join( "^", ObservationIdentifier );
+
+                                    if ( !_obx.Contains( id_test ) )
+                                    {
+                                        logobx += String.Join( "|", /*obx_item.OBX_split_heder*/tmp_ ) +
+                                                  Environment.NewLine;
+                                        _obx.Add( id_test );
+                                    }
+                                }
+
                             }
 
-                            if ( logobx.Length > 0 )
+                            if ( logobx.Length > 0  )
                             {
                                 String msgstr = msg_item.MSH_line + Environment.NewLine + msg_item.PID_line +
                                                 Environment.NewLine + obr_item.OBR_line + Environment.NewLine +
@@ -218,6 +238,7 @@ namespace cmdHL7Parse
                                 log( msgstr );
 
                                 Historylog( msg_item.PID_split[ 2 ] + "|" + test );
+                                
                             }
                         }
                     }
@@ -225,7 +246,7 @@ namespace cmdHL7Parse
             }
         }
 
-        static bool PregnancyTest(string[] confog_line, List<OBR> OBRs)
+        private static bool PregnancyTest( string[] confog_line, List<OBR> OBRs )
         {
             bool result = ( confog_line.Count() > 0 );
 
